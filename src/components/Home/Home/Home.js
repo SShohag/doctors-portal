137109ceb172dda7c5ef445b35a52f2cf5706e97
SShohag <shohag7m@gmail.com>
2020-10-10 20:17:04 +0600

    dashboard appointment login

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Blogs from '../Blogs/Blogs';
-import Contact from '../Contact/Contact';
-import Doctors from '../Doctors/Doctors';
 import FeaturedService from '../FeaturedService/FeaturedService';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
 import MakeAppointment from '../MakeAppointment/MakeAppointment';
-import Services from '../Services/Services';
 import Testimonials from '../Testimonials/Testimonials';
+import Header from '../Header/Header';
+import Services from '../Services/Services';
+import Blogs from '../Blogs/Blogs';
+import Doctors from '../Doctors/Doctors';
+import Contact from '../Contact/Contact';
+import Footer from '../../Shared/Footer/Footer';
 
 const Home = () => {
     return (
---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDh_blQTNmlmavyWrMEU9yghzRoKmN-IKQ",
+    authDomain: "doctor-portal-main.firebaseapp.com",
+    databaseURL: "https://doctor-portal-main.firebaseio.com",
+    projectId: "doctor-portal-main",
+    storageBucket: "doctor-portal-main.appspot.com",
+    messagingSenderId: "887413665430",
+    appId: "1:887413665430:web:367f524a959b00370b5cb7"
+  };
+
+  export default firebaseConfig;
